ee6976647fd25903bbc5c05ad9d70f0884b2e400
Andrew Paige <keeperpaige@gmail.com>
2024-02-19 19:11:43 -0500

    change host

--- app.py
+++ app.py
@@ -115,4 +115,4 @@ def home():
     return render_template("home.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4), user_docs=[])
 
 if __name__ == "__main__":
-    app.run(port=3000)
+    app.run(host='0.0.0.0')
